3178cf602143f49a18c0746a94a15ace0c37c5ce
wollenet <ewollenberg16@jcu.edu>
2018-02-05 21:49:46 -0500

    fixing heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,15 +1,4 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
 
-
-var app = express();
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
 
 app.post("/api/new", function(req, res) {
     var newFriend = req.body;
@@ -22,6 +11,4 @@ app.get("/api/friends", function(req, res) {
     res.json(friendsArr);
 });
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-});
+
